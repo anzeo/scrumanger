52ef5bd0a2aed8110dd4c72d775a9a3d4298865e
Thomas Anciaux <zebigbozz@gmail.com>
2015-09-20 18:46:32 +0200

    Save plan changes

--- src/app/plan/scripts/plan-controller.js
+++ src/app/plan/scripts/plan-controller.js
@@ -1,3 +1,12 @@
-angular.module('scrumanger').controller('PlanController', function(plan){
-    this.plan = plan;
+angular.module('scrumanger').controller('PlanController', function($scope, AppService, plan){
+    var controller = this;
+    controller.plan = plan;
+
+    $scope.$watch(function(){
+        return JSON.stringify(controller.plan);
+    }, function(newValue, oldValue){
+        if(oldValue && oldValue !== newValue){
+            AppService.put(controller.plan._links.self, controller.plan);
+        }
+    });
 });
