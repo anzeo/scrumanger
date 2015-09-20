1da3ee22f685e1f5fc09d9ee3a63b834e360af7a
Thomas Anciaux <zebigbozz@gmail.com>
2015-09-20 11:10:52 +0200

    Make ticket require lane

--- src/ticket/scripts/ticket.js
+++ src/ticket/scripts/ticket.js
@@ -1,12 +1,14 @@
 angular.module('scrumanger.ticket').directive('ticket', function($templateCache){
    return {
        restrict: 'E',
+       require: '^lane',
        scope: {
            title: '@'
        },
        controller: 'TicketController',
        controllerAs: 'Ticket',
        bindToController: true,
-       template: $templateCache.get('ticket/views/ticket.html')
+       template: $templateCache.get('ticket/views/ticket.html'),
+       link: function (scope, element, attrs, LaneController) {}
    }
 });
